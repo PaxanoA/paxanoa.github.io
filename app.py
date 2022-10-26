614e8baafa2a21582f0191a37366647fe46652f7
PaxanoA <pachanoandres2@gmail.com>
2022-10-26 17:08:04 -0500

    Mini Update

--- app.py
+++ app.py
@@ -1,26 +1,12 @@
 from flask import Flask, render_template, request
 
-app= Flask(__name__, template_folder="templates")
+app= Flask(__name__, template_folder="paxanoa.github.io")
 
 @app.route('/')
 def index():
 	return render_template('index.html')
 
-@app.route('/about')
-def about():
-	return render_template('about.html')
 
-@app.route('/contact-me')
-def contact():
-	return render_template('contact.html')
-
-@app.route('/resume')
-def resume():
-	return render_template('resume.html')
-
-@app.route('/donations')
-def donations():
-	return render_template('donations.html')
 
 
 if __name__ == '__main__':
